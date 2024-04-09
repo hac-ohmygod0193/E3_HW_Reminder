b41601492d06e1f86d11d35d66d259c0cade1fdb
ohmygod0193 <62164142+hac-ohmygod0193@users.noreply.github.com>
2024-04-09 23:44:52 +0800

    change from gemini api to groq api

--- main.py
+++ main.py
@@ -1,35 +1,73 @@
 import requests
 from bs4 import BeautifulSoup
-from langchain_google_genai import (
-    ChatGoogleGenerativeAI,
-    HarmBlockThreshold,
-    HarmCategory,
-)
-import google.generativeai as genai
 import os
 import datetime
 import re
+from groq import Groq
+import random
+
+groq_api_keys = [
+    os.environ.get("GROQ_API_KEY"),
+    os.environ.get("GROQ_API_KEY_1"),
+    os.environ.get("GROQ_API_KEY_2"),
+]
+selected_api_key = random.choice(groq_api_keys)
+client = Groq(
+    api_key=selected_api_key,
+)
+model_mapping = {
+    "0": "llama2-70b-4096",
+    "1": "mixtral-8x7b-32768",
+    "2": "gemma-7b-it",
+    "3": "gemini-pro",
+}
+
+
 current_time = datetime.datetime.now()
 current_date = current_time.date()
-print(current_time)
 # Load environment variables
 e3p_cookie = os.environ["E3P_COOKIE"]
 GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
 line_notify_token = os.environ["LINE_NOTIFY_TOKEN"]
-
-# Configure generative AI
-genai.configure()
-gemini_pro = ChatGoogleGenerativeAI(
-    model="gemini-pro",
-    temperature=0.1,
-    safety_settings={
-        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
-        HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
-        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
-        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
-    },
-)
-
+def groq_api(llm_prompt: str):
+    select_model = "1"
+    model = model_mapping[select_model]
+    chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": str(llm_prompt),
+                    
+                }
+            ],
+            model=model,temperature = 0.0,
+    )
+    response = chat_completion.choices[0].message.content
+    if model == "gemma-7b-it":
+        response = response.replace('*', '').replace('-', '')
+    return response
+def gemini_api(llm_prompt: str):
+    from langchain_google_genai import (
+        ChatGoogleGenerativeAI,
+        HarmBlockThreshold,
+        HarmCategory,
+    )
+    import google.generativeai as genai
+    # Configure generative AI
+    genai.configure()
+    gemini_pro = ChatGoogleGenerativeAI(
+        model="gemini-pro",
+        temperature=0.1,
+        safety_settings={
+            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+        },
+    )
+    response = gemini_pro.invoke(llm_prompt).content
+    response = response.replace('*', '').replace('-', '')
+    return response
 def lineNotifyMessage(token, msg):
     headers = {
         "Authorization": "Bearer " + token,
@@ -42,22 +80,23 @@ def lineNotifyMessage(token, msg):
 
 
 prompt = """
-Announcement:
 ---------------------
 {message}
 ---------------------
-
-Summarize this homework announcement for me,
-highlighting deadlines and extracting important information as clearly and concisely as possible.
-Use only headers and bullet points in your summary.
-Use Traditional Chinese only.
+Summarize this homework announcement for me in Traditional Chinese and less than 50 words.
+Use only bullet points to summarize the announcement.
+Don't include any unnecessary information.
+公告內容摘要如下:
 """
+
 
 def send_e3_hw_announcement(url: str):
     """Fetch the page content"""
     headers = {
-        "Cookie": e3p_cookie,
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        "Cookie":
+        e3p_cookie,
+        "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
     }
     requests.adapters.DEFAULT_RETRIES = 5
     html = requests.get(url, headers=headers).text
@@ -66,20 +105,21 @@ def send_e3_hw_announcement(url: str):
         f.write(html)
 
     soup = BeautifulSoup(html, "html.parser")
-    headlines = soup.find_all(name="h3", attrs={"class": "name d-inline-block"})
+    headlines = soup.find_all(name="h3",
+                              attrs={"class": "name d-inline-block"})
     tags = soup.find_all(name="div", attrs={"class": "event mt-3"})
     final_message = ""
     prefix_message = "\n今天日期: " + str(
         current_date) + '\n' + "近三日的作業公告\n" + '=' * 16 + '\n'
     more_than_n_days = False
-    hw_time_delta = 3
+    hw_time_delta = 7
     for i in range(len(tags)):
         block = tags[i]
         headline = headlines[i]
         message = "\n"
         block_title = block.find_all(name="div", attrs={"class": "col-11"})
         message += headline.text + '\n' + '截止時間:\n'
-        for title in block_title:
+        for title in block_title[:2]:
             if title.text == '課程事件':
                 continue
             date_pattern = r"\d{2}月 \d{2}日"
@@ -102,10 +142,13 @@ def send_e3_hw_announcement(url: str):
             message += title.text + '\n'
         if more_than_n_days:
             break
-        llm_prompt = prompt.format(message=message)
-        #response = gemini_pro.invoke(llm_prompt).content
-        #response = response.replace('*', '').replace('-', '')
-        final_message += (message + '\n' + '=' * 16 + '\n')
+        if block_title[2].text == '\n':
+            response = ""
+        else:
+            llm_prompt = prompt.format(message=block_title[2].text)
+            response = groq_api(llm_prompt)
+            message += '\n'+ response + '\n'
+        final_message += (message + '\n課程:\n' + block_title[-1].text[12:]+'\n'+'=' * 16 + '\n')
     if (final_message == ""):
         final_message = "\n恭喜! 近三日無作業公告"
     else:
